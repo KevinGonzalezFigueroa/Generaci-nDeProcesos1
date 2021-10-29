22ebc714b5fdff24864d2bfc905257d2dfe85069
Kevin González <usr2282@salesianos-lacuesta.net>
2021-10-29 09:47:36 +0100

    Feature: Se ha realizado la actividad. Se pide como parámetros direccionWeb y numeroDeVeces, y se crea un proceso con lo que le indicamos que abra Firefox y abra una nueva ventana con la dirección web indicada, se abre tantas veces como le indiquemos y esperamos a que el proceso muera para terminar. Esta vez arreglado.

--- src/com/generaciondeprocesos/Main.java
+++ src/com/generaciondeprocesos/Main.java
@@ -1,6 +1,7 @@
 package com.generaciondeprocesos;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,18 +13,23 @@ public class Main {
         String direccionWeb = sc.nextLine();
         System.out.println("¿Cuántas veces quieres que se abra?");
         int numeroDeVeces = sc.nextInt();
+        ArrayList<Process> lista = new ArrayList<>();
 
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("C:\\Program Files\\Mozilla Firefox\\firefox.exe", "/c", direccionWeb);
+        processBuilder.command("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\95.0.1020.30\\msedge.exe", direccionWeb, "--new-window");
 
         try {
             Process process = processBuilder.start();
+            lista.add(process);
 
-            for (int i = 0; i < numeroDeVeces-1; i++) {
+            for (int i = 0; i <= numeroDeVeces-2; i++) {
                 process = processBuilder.start();
+                lista.add(process);
             }
 
-            process.waitFor();
+            for (Process processFor: lista){
+                processFor.waitFor();
+            }
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
